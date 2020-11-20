0f4a6d97f471476ac1e859a04ee7c45a8c4c1293
MacBookPro <macbookpro@MacBookPro.local>
2020-11-20 17:38:26 +0330

    create.calculator

---
+++ gitp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+float num1 , num2 , result ; 
+int function;
+printf("pleas print your two numbers ,sprate with space\n");
+scanf("%f %f",&num1,&num2);
+printf("please choose your function\n 1=plus \n 2=mines\n");
+scanf("%d",&function);
+if (function==1){
+    result = num1 + num2;
+}
+if (function==2){
+    result=num1-num2;
+}
+printf("your result is= %f\n",result);
+return 0;
+}
