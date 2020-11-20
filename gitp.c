0ec16094588775dd0cdb0b8554a7bdc65b5078ab
MacBookPro <macbookpro@MacBookPro.local>
2020-11-20 18:25:08 +0330

    add.taghsim&add.command

--- gitp.c
+++ gitp.c
@@ -1,20 +1,29 @@
 #include<stdio.h>
 int main(){
+    /* tarif moteghayer ha */
 float num1 , num2 , result ; 
-int function;
+int function; 
+
+/* gereftan adad az karbar*/
 printf("pleas print your two numbers ,sprate with space\n");
 scanf("%f %f",&num1,&num2);
-printf("please choose your function\n 1=plus \n 2=mines\n 3=zarb\n");
+printf("please choose your function\n 1=plus \n 2=mines\n 3=zarb\n 4=taghsim\n");
 scanf("%d",&function);
-if (function==1){
+
+/*function*/
+if (function==1)
     result = num1 + num2;
-}
-if (function==2){
+
+if (function==2)
     result=num1-num2;
-}
-if (function==3){
+
+if (function==3)
     result=num1*num2;
-}
+
+if (function==4)
+    result=num1/num2;
+
+/*result*/
 printf("your result is= %f\n",result);
-return 0;
-}
+
+return 0;}
